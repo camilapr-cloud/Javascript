4236737d8e25fc7e663a17a737c06354f6769de5
camilapr-cloud <80237078+camilapr-cloud@users.noreply.github.com>
2021-03-25 20:09:27 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 
-var myVariable = 1;
-let myVariable2 = 3;
+let myName = prompt ("Ingrese su nombre");
+console.log(myName);
+alert("Hola" + myName );
 
-let resultado = myVariable + myVariable2;
-let resultadoConString = MYCONSTANT + resultado;
+let myAge = parseInt (prompt ("Ingrese su edad para ganarse una cerveza gratis"));
+
+if (myAge < 18) {
+    alert ("sos menos de edad gil! como te vamos a regalar una cerveza")
+}
+
+else {
+    alert ("Hurra!, estas viejo y te llevas una birra presentando tu documento")
+}
